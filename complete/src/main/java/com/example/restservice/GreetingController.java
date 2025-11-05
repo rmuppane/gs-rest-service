b18890d18109ed5aef37eee1cb8fbd2520d85e6c
Ramakrishna Muppaneni <51655061+rmuppane@users.noreply.github.com>
2025-11-05 11:28:12 +0000

    Update GreetingController.java

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -11,6 +11,6 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return new Greeting(counter.incrementAndGet(), String.format(template, name))
 	}
 }
